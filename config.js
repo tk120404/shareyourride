526e446ca40910274207ec2b20a1865655df5986
Arjunkumar Krishnamoorthy <arjunkumartk@gmail.com>
2013-10-27 08:09:47 +0530

    initial commit with share and search

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'app':{
+        'port':8080
+    },
+    'db':{
+        'name':'shareurride',
+        'host':'localhost',
+        'port':27017
+    }
+
+};
---
+++ start.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    logic = require('./lib/logic')
+    config = require('./config'),
+    mongo = require('mongodb'),
+    monk = require('monk'),
+    db = monk(config.db.host+':'+config.db.port+'/'+config.db.name),
+    ride = db.get('ride');
+
+
+
+
+//express configuration
+app.configure(function () {
+    app.set('port', config.app.port);
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+
+});
+
+
+app.post('/share', logic.share(ride));
+
+app.post('/search',logic.search(ride));
+
+
+app.get('/circle', function (req, res) {
+
+
+    res.end('Hello');
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port')+ " " +Date(Date.now()));
+});
